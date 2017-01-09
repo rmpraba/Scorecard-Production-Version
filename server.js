316c87e802ffdc88d2c28e6965b894b9a59ee947
janani1995 <cutesuja24@gmail.com>
2017-01-09 22:19:18 +0530

    alignment3

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express    = require("express");
  var mysql      = require('mysql');
  var email   = require("emailjs/email");
@@ -11,7 +12,7 @@ var connection = mysql.createConnection({
    // port     : '58116',
    // user     : 'adminTEZN77',
    // password : 'pEbElHqKSxh2',
-   database : 'scorecarddatabase'
+   database : 'scorecarddb'
  });
 var bodyParser = require('body-parser');
 var app = express();
