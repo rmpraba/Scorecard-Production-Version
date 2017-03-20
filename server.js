974e0b4cc505faa0904d63741a9e9146842e9a56
janani1995 <cutesuja24@gmail.com>
2017-03-20 09:49:46 +0530

    recent changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
    password : 'admin',
-   database : 'demoscdb'
+   database : 'demo1'
 
  });
 var bodyParser = require('body-parser'); 
