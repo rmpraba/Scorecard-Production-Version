de17462383448604e8d95c05d8331e933d2fc079
janani1995 <cutesuja24@gmail.com>
2017-04-15 19:18:15 +0530

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
    password : 'admin',
-   database : 'reportcardmaster'
+   database : 'scdemo'
  });
 
 
